db5c29a6a08abd54f18e3f6f0295e54b9b075403
indianku <106584664+indianku@users.noreply.github.com>
2022-06-08 13:17:44 +0530

    Update LoginPage.java

--- flipkartautomation/src/main/java/pomClasses/LoginPage.java
+++ flipkartautomation/src/main/java/pomClasses/LoginPage.java
@@ -11,8 +11,8 @@ public class LoginPage {
 	@FindBy(xpath="//input[contains(@class,'_2IX_2- _3mctLh VJ')]")WebElement password;
 	@FindBy(xpath="//button[@class='_2KpZ6l _2HKlqd _3AWRsL']")WebElement login;
 	public LoginPage(WebDriver driver) {PageFactory.initElements(driver, this);}
-	public void enterUser() {user.sendKeys("7709395679");}
-	public void enterPassword() {password.sendKeys("pra13nay");}
+	public void enterUser() {user.sendKeys("0000000000");}
+	public void enterPassword() {password.sendKeys("abcdef");}
 	public void selectLogin() {login.click();}
 	
 }
